1865a6add04ec6724ec1370fda924d0077aa34cd
Josh Holman <git@jolman.me>
2026-01-25 14:44:38 -0800

    implement auth server-side

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -1,5 +1,6 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
+import { bearerAuth } from 'hono/bearer-auth';
 import { LinkCreate } from "./endpoints/linkCreate";
 import { LinkDelete } from "./endpoints/linkDelete";
 import { LinkFetch } from "./endpoints/linkFetch";
@@ -9,6 +10,14 @@ import { LinkUpdate } from "./endpoints/linkUpdate";
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
 
+// TODO: Add functionality to skip this in local dev
+app.use('/links/*', async (c, next) => {
+	const auth = bearerAuth({
+		token: c.env.SHORTER_API_KEY,
+	});
+	return auth(c, next)
+})
+
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
 	docs_url: "/",
@@ -17,8 +26,8 @@ const openapi = fromHono(app, {
 // Register OpenAPI endpoints
 openapi.post("/links", LinkCreate);
 openapi.put("/links/:slug", LinkUpdate);
-openapi.get("/links", LinkList);
-openapi.get(":slug", LinkFetch);
+openapi.get("/", LinkList);      // public
+openapi.get(":slug", LinkFetch); // public
 openapi.delete("/links/:slug", LinkDelete);
 
 // You may also register routes for non OpenAPI directly on Hono
