4b5ba20c344c4386dcfb4d5e295cb20c41f26b95
josh <git@jolman.me>
2026-01-26 12:20:27 -0800

    render html

--- packages/service/src/endpoints/linkList.ts
+++ packages/service/src/endpoints/linkList.ts
@@ -1,5 +1,6 @@
 import { Bool, OpenAPIRoute } from "chanfana";
 import { z } from "zod";
+import { html } from 'hono/html';
 import { type AppContext, Link, KvEntry } from "../types";
 
 export class LinkList extends OpenAPIRoute {
@@ -26,7 +27,28 @@ export class LinkList extends OpenAPIRoute {
 	};
 
 	async handle(c: AppContext) {
-		const allLinks = await c.env.KV_SHORTLINKS.get<KvEntry[]>("list", "json")
+		const allLinks = await c.env.KV_SHORTLINKS.get<KvEntry[]>("list", "json") || []; // null check
+
+		// Return HTML if the client requesting wants html (e.g., browsers) and JSON otherwise
+		const accept = c.req.header("Accept");
+		if (accept && accept.includes('text/html')) {
+			return c.html(html`
+<html>
+	<head><title>acm@CSUF's Shortlinks</title></head>
+	<body>
+		<div style="margin: auto;">
+			<h1>Shortlink Mappings</h1>
+			<ul>
+				${allLinks.map(link => `
+					<li><a href="https://s.acmcsuf.com/${link.key}">${link.key}</a> => <a href="${link.value.url}">${link.value.url}</a></li>
+				`).join('')}
+			</ul>
+		</div>
+	</body>
+</html>
+`
+			)
+		}
 
 		return c.json({
 			success: true,
