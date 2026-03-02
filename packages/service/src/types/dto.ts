2b29ad921918322555e2652fe062a48578f61ec0
josh <git@jolman.me>
2026-03-01 22:22:45 -0800

    ok make it actually work

--- packages/service/src/types/dto.ts
+++ packages/service/src/types/dto.ts
@@ -21,6 +21,6 @@ export const ShortlinkModel = z.object({
 	slug: z.string(),
 	url: z.url(),
 	isPermanent: z.boolean().optional().default(false),
-	createdAt: z.iso.datetime(),
-	updatedAt: z.iso.datetime(),
+	createdAt: z.iso.datetime().optional(),
+	updatedAt: z.iso.datetime().optional(),
 })
