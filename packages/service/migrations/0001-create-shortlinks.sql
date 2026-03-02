647f5ed00428682fa7649e6706a942dcf4475683
josh <git@jolman.me>
2026-03-01 21:53:25 -0800

    actually use this simpler schema for now

--- packages/service/migrations/0001-create-shortlinks.sql
+++ packages/service/migrations/0001-create-shortlinks.sql
@@ -1,16 +1,7 @@
-CREATE TABLE links (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  url TEXT NOT NULL,
-  permanent INTEGER NOT NULL DEFAULT 0, -- boolean
-  created_at TEXT NOT NULL DEFAULT (datetime('now'))
-  updated_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
-
-CREATE TABLE slugs (
+CREATE TABLE shortlinks (
   slug TEXT PRIMARY KEY,
-  link_id INTEGER NOT NULL REFERENCES links(id) ON DELETE CASCADE,
-  is_primary INTEGER NOT NULL DEFAULT 0, -- boolean, marks 'canonical' slug vs alias
-  careated_at TEXT NOT NULL DEFAULT (datetime('now'))
+  url TEXT NOT NULL,
+  isPermanent INTEGER NOT NULL DEFAULT 0, -- boolean
+  createdAt TEXT NOT NULL DEFAULT (datetime('now')),
+  updatedAt TEXT NOT NULL DEFAULT (datetime('now'))
 );
-
-CREATE INDEX idx_slugs_link_id ON slugs(link_id);
