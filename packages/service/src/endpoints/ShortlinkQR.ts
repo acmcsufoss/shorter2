1a11cca6e2f22b565211b70e80ef8e2b45fb2c70
Josh Holman <git@jolman.me>
2026-03-02 13:42:15 -0800

    add qr code gen

---
+++ packages/service/src/endpoints/ShortlinkQR.ts
@@ -0,0 +1,58 @@
+import { OpenAPIRoute } from "chanfana";
+import { z } from "zod";
+import { getShortlinkBySlug } from "../repository";
+import type { AppContext } from "../types";
+import QRCode from 'qrcode-svg';
+
+export class ShortlinkQR extends OpenAPIRoute {
+	schema = {
+		tags: ["Public"],
+		summary: "Get QR code for a shortlink",
+		request: {
+			params: z.object({
+				slug: z.string(),
+			}),
+		},
+		responses: {
+			"200": {
+				description: "SVG QR Code",
+			},
+			"404": {
+				description: "Slug not found",
+				content: {
+					"application/json": {
+						schema: z.object({
+							series: z.object({
+								success: z.boolean(),
+								error: z.string(),
+							}),
+						}),
+					},
+				},
+			},
+		},
+	};
+
+	async handle(c: AppContext) {
+		// Get validated data
+		const data = await this.getValidatedData<typeof this.schema>();
+		const rawSlug = data.params.slug;
+		const slug = rawSlug.replace("/\.svg$/", "");
+
+		const res = await getShortlinkBySlug(c, slug);
+		if (!res) {
+			return c.json(
+				{
+					success: false,
+					error: "Slug not found",
+				},
+				404,
+			);
+		}
+		const targetUrl = `https://${new URL(c.req.url).hostname}/${slug}`;
+		const QRCode = require("qrcode-svg");
+		const svg = new QRCode(targetUrl).svg();
+
+		return c.body(svg, 200, { "Contenty-Type": "image/svg+xml" });
+	}
+}
