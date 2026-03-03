ef2885fbe50a0a7e66d77fca4e3fb3cec8b05a3d
Josh Holman <git@jolman.me>
2026-03-02 19:26:42 -0800

    ephemeral messages and better try/catch

--- packages/types/src/dto.ts
+++ packages/types/src/dto.ts
@@ -6,12 +6,14 @@ export const ShortlinkCreateRequest = z.object({
 	isPermanent: z.boolean().optional().default(false),
 });
 export type ShortlinkCreateRequest = z.infer<typeof ShortlinkCreateRequest>
+export type ShortlinkCreateRequestInput  = z.input<typeof ShortlinkCreateRequest>
 
 export const ShortlinkUpdateRequest = z.object({
 	url: z.url().optional(),
 	isPermanent: z.boolean().optional(),
 });
 export type ShortlinkUpdateRequest = z.infer<typeof ShortlinkUpdateRequest>
+export type ShortlinkUpdateRequestInput  = z.input<typeof ShortlinkUpdateRequest>
 
 export const ShortlinkModel = z.object({
 	slug: z.string(),
