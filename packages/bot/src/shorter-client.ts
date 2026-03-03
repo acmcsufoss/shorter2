d99ae2651ad19b406b62fa69f3659cdabd11c1fb
Josh Holman <git@jolman.me>
2026-03-02 19:04:28 -0800

    organize client into class

--- packages/bot/src/shorter-client.ts
+++ packages/bot/src/shorter-client.ts
@@ -5,65 +5,69 @@ import type {
 	ShortlinkUpdateRequest
 } from "./types";
 
-const endpoint = `${env.SHORTER_ENDPOINT}/_links`;
+export class ShortlinkClient {
+	private authToken = ""
+	private endpoint = `${env.SHORTER_ENDPOINT}/_links`;
 
-const setHeaders = (authToken: string) => {
-	return {
-		Authorization: `Bearer ${authToken}`,
-		"Content-Type": "application/json",
+	constructor(authToken: string) {
+		this.authToken = authToken
+	}
+
+	setHeaders() {
+		return {
+			Authorization: `Bearer ${this.authToken}`,
+			"Content-Type": "application/json",
+		};
 	};
-};
 
-export async function addLink(
-	shortlink: ShortlinkCreateRequest,
-	authToken: string,
-): Promise<ShortlinkModel> {
-	const response = await fetch(endpoint, {
-		method: "POST",
-		headers: setHeaders(authToken),
-		body: JSON.stringify(shortlink),
-	});
+	async post(
+		shortlink: ShortlinkCreateRequest,
+	): Promise<ShortlinkModel> {
+		const response = await fetch(this.endpoint, {
+			method: "POST",
+			headers: this.setHeaders(),
+			body: JSON.stringify(shortlink),
+		});
 
-	if (!response.ok) {
-		const errText = await response.text();
-		throw new Error(`HTTP ${response.status}: ${errText}`);
+		if (!response.ok) {
+			const errText = await response.text();
+			throw new Error(`HTTP ${response.status}: ${errText}`);
+		}
+
+		return (await response.json()) as ShortlinkModel;
 	}
 
-	return (await response.json()) as ShortlinkModel;
-}
+	async delete(
+		slug: string,
+	): Promise<void> {
+		const deleteUrl = `${this.endpoint}/${slug}`;
+		const response = await fetch(deleteUrl, {
+			method: "DELETE",
+			headers: this.setHeaders(),
+		});
 
-export async function deleteLink(
-	slug: string,
-	authToken: string,
-): Promise<void> {
-	const deleteUrl = `${endpoint}/${slug}`;
-	const response = await fetch(deleteUrl, {
-		method: "DELETE",
-		headers: setHeaders(authToken),
-	});
+		if (!response.ok) {
+			const errText = await response.text();
+			throw new Error(`HTTP ${response.status}: ${errText}`);
+		}
+	}
 
-	if (!response.ok) {
-		const errText = await response.text();
-		throw new Error(`HTTP ${response.status}: ${errText}`);
+	async put(
+		slug: string,
+		updateParams: ShortlinkUpdateRequest,
+	): Promise<ShortlinkModel> {
+		const updateUrl = `${this.endpoint}/${slug}`;
+		const response = await fetch(updateUrl, {
+			method: "",
+			headers: this.setHeaders(),
+			body: JSON.stringify(updateParams),
+		});
+		if (!response.ok) {
+			const errText = await response.text();
+			throw new Error(`HTTP ${response.status}: ${errText}`);
+		}
+
+		return (await response.json()) as ShortlinkModel;
 	}
 }
 
-export async function updateLink(
-	slug: string,
-	updateParams: ShortlinkUpdateRequest,
-	authToken: string,
-): Promise<ShortlinkModel> {
-	const updateUrl = `${endpoint}/${slug}`;
-	const response = await fetch(updateUrl, {
-		method: "",
-		headers: setHeaders(authToken),
-		body: JSON.stringify(updateParams),
-	});
-
-	if (!response.ok) {
-		const errText = await response.text();
-		throw new Error(`HTTP ${response.status}: ${errText}`);
-	}
-
-	return (await response.json()) as ShortlinkModel;
-}
