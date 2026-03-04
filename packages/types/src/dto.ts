a3abe1cdd1cbff4b4a681a1b9282fd43e169ccd5
Josh Holman <git@jolman.me>
2026-03-04 09:38:59 -0800

    update schema to require web urls

--- packages/types/src/dto.ts
+++ packages/types/src/dto.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const ShortlinkCreateRequest = z.object({
 	slug: z.string(),
-	url: z.url(),
+	url: z.httpUrl(),
 	isPermanent: z.boolean().optional().default(false),
 });
 export type ShortlinkCreateRequest = z.infer<typeof ShortlinkCreateRequest>
 export type ShortlinkCreateRequestInput  = z.input<typeof ShortlinkCreateRequest>
 
 export const ShortlinkUpdateRequest = z.object({
-	url: z.url().optional(),
+	url: z.httpUrl().optional(),
 	isPermanent: z.boolean().optional(),
 });
 export type ShortlinkUpdateRequest = z.infer<typeof ShortlinkUpdateRequest>
@@ -17,7 +17,7 @@ export type ShortlinkUpdateRequestInput  = z.input<typeof ShortlinkUpdateRequest
 
 export const ShortlinkModel = z.object({
 	slug: z.string(),
-	url: z.url(),
+	url: z.httpUrl(),
 	isPermanent: z.boolean().optional().default(false),
 	createdAt: z.iso.datetime().optional(),
 	updatedAt: z.iso.datetime().optional(),
